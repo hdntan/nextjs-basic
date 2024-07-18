878637feac96343edd5208dc35694db386cedfe0
= <=>
2024-07-18 16:22:36 +0700

    feat: add dark mode

---
+++ src/app/(landing)/(homepage)/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/custome/ModeToggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center  p-24">
+     <ModeToggle/>
+     <div>hahah</div>
+    </main>
+  );
+}
